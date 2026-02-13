d784dab91ed669587321d5ccaec833876db37baf
denzhe dzebu <dedzebujhb024@student.wethinkcode.co.za>
2026-02-13 09:59:14 +0200

    initial spring setup

---
+++ src/test/java/com/NoQ/noqueue/NoqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoQ.noqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
